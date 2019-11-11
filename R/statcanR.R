5ba541e55341bf548b41fd823e2aed45b0ce9025
Martin Paquette <paquettem@skemalab.io>
2019-11-11 21:53:19 +0000

    u

---
+++ R/statcanR.R
@@ -0,0 +1,112 @@
+
+#' statcanR
+#'
+#'
+#'Get all Canadian statistics data (CANSIM tables) now identify by Product IDs (PID) by the new Statistics Canada Web Data Service
+#'
+#'The
+#'\code{_getCansimTbl_} function has 2 arguments to fulfill to get data: {_tableNumber_} & param{_lang_}
+#'
+#'
+#'The _tableNumber_ argument simply referes to the table number of the Statistics Canada data table you want to collect,
+#'such as '_14-10-0287-03_' for the _Labour force characteristics by province, monthly, seasonally adjusted_ as an example.
+#'
+#'To get the table number :\url{https://www150.statcan.gc.ca/n1/en/type/data}
+#'
+#'The second argument, _lang_, referes to the language. As Canada is a billingual country (french speaking in Quebec and english speaking in the other provinces), Statistics Canada display all Statistics Data in both languages.
+#' Therefore, users can choose if they want to get satistics data table in french or english by seting the _lang_ argument by c("fra", "eng").
+#'
+#'
+#' @param tableNumber The table number of the Statistics Canada data table
+#' @param lang The language wanted
+#'
+#' @return The output will be a data table representing the data associated with the chosen table number
+#' @export
+#'
+#'@import data.table
+#'
+#'
+#' @examples
+#' \dontrun{
+#' datatable <- getCansimTble("14-10-0287-03","eng")
+#'}
+
+# Scrapping function for statcan
+getCansimTbl <- function(tableNumber, lang){
+
+
+  #table number
+  tableNumber <- gsub("-", "", substr(tableNumber, 1, nchar(tableNumber)-2))
+
+  #create path
+  path <- "./"
+
+  #create a temporary folder to manipulated the scraped data
+  dir.create(file.path(path, "temp"))
+
+  #get data in english version
+  if(lang == "eng"){
+    #download the data file in english version
+    urlEng <- paste0("https://www150.statcan.gc.ca/n1/en/tbl/csv/",
+                     tableNumber,
+                     "-eng.zip")
+    utils::download.file(urlEng,
+                  destfile=paste0(path,"/temp/datasetEng.zip"),
+                  method="curl")
+
+    #unzip the downloaded data file in english version
+    utils::unzip(paste0(path, "/temp/datasetEng.zip"),
+          exdir = paste0(path,"/temp"))
+
+    #load the data file in english version
+    data <- fread(paste0(path,
+                         "/temp/",
+                         tableNumber,
+                         ".csv"))
+
+    #add to the data.frame or data.table the Official Data Table Indicator
+    #defined by Statitics Canada and based on meta data file.
+    data$INDICATOR <- as.character(0)
+    data$INDICATOR <- as.character(utils::read.csv(paste0(path,
+                                                   "/temp/",
+                                                   tableNumber,
+                                                   "_MetaData.csv")
+    )[1,1])
+  }
+
+  #get data in french version
+  if(lang == "fra"){
+    #download the data file in french version
+    urlFra <- paste0("https://www150.statcan.gc.ca/n1/fr/tbl/csv/",
+                     tableNumber,
+                     "-fra.zip")
+    utils::download.file(urlFra,
+                  destfile=paste0(path, "/temp/datasetFra.zip"),
+                  method="curl")
+
+    #unzip the downloaded data file in french version
+    utils::unzip(paste0(path, "/temp/datasetFra.zip"),
+          exdir = paste0(path,"/temp"))
+
+    #load the data file in french version
+    data <- fread(paste0(path,
+                         "/temp/",
+                         tableNumber,
+                         ".csv"))
+    #add to the data.frame or data.table the Official Data Table Indicator
+    #defined by Statitics Canada and based on meta data file.
+    data$INDICATEUR <- as.character(0)
+    data$INDICATEUR <- as.character(utils::read.csv(paste0(path,
+                                                    "/temp/",
+                                                    tableNumber,
+                                                    "_MetaData.csv")
+    )[1,1])
+  }
+
+  unlink(paste0(path,"/temp/"), recursive = TRUE)
+
+  return(data)
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# pkgdown <img src="man/figures/LOGO.png" align="right" />
+
+# statcanR
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of statcanR is to get all to get all Canadian statistics data
+(CANSIM tables,now identify by Product IDs (PID)) without any limitation
+and provided by the new Statistics Canada Web Data Service
+
+## Installation
+
+You can install the released version of statcanR from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("statcanR")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(statcanR)
+data <- getCansimTbl("14-10-0063-01", "eng")
+```
+
+## SKEMA Global Lab in AI
+
+For more information on the SKEMA Global Lab in AI or the SKEMA Quantum Studio, you can visit our [website](https://skemagloballab.io/).
