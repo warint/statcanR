980adb7a8a9488038c8599477dc19599acabaaca
Thierry Warin <thierry.warin@nuance-r.com>
2023-02-15 13:46:05 -0500

    u

--- R/statcan_search.R
+++ R/statcan_search.R
@@ -1,10 +1,25 @@
+# downloading the data file in English version
+url <- paste0("https://warin.ca/datalake/statcanR/statcan_data.qs")
+
+if (httr::http_error(url)) 
+{ # network is down = message (not an error anymore)
+  message("No tables with this combination of keywords")
+} else{ 
+  path <- file.path(tempdir(), "temp.qs")
+  curl::curl_download(url, path)
+  qs_file <- file.path(paste0(tempdir(), "/temp.qs"))
+  statcandata <- qs::qread(qs_file)
+  
+}
+
+
 #' Searching function for statcanR
 #'
 #'
 #' Easily connect to Statistics Canada's Web Data Service with R. Open economic data (formerly known as CANSIM tables, now identified by Product IDs (PID)) are accessible as a data frame, directly in the user's R environment.
 #'
 #'
-#' @description The \code{statcan_search()} function has 1 arguments to fulfill to find a database: {keywords}.
+#' @description The \code{statcan_search()} function has 2 arguments to fulfill to find a database: {keywords} and {lang}.
 #' The keywords argument refers to words that can be found in either the title or the description of the database. For example, inserting the keywords
 #' "economy","export",and "link" will bring up the title, table id, description, and release date for databases that include these keywords. In this case, only one data table ("Supply and use tables, link-1997 level")
 #' would be returned as it is the only data table containing all three words.
@@ -21,25 +36,10 @@
 #' @import curl
 #' @import qpdf
 #' @import qs
-#' @examples 
-#' statcan_search(c("economy","export","link"),"eng")
+#' @examples statcan_search(c("economy","export","link"),"eng")
 
-# downloading the data file in English version
-url <- paste0("https://warin.ca/datalake/statcanR/statcan_data.qs")
 
-if (httr::http_error(url)) 
-{ # network is down = message (not an error anymore)
-  message("No tables with this combination of keywords")
-} else{ 
-  path <- file.path(tempdir(), "temp.qs")
-  curl::curl_download(url, path)
-  qs_file <- file.path(paste0(tempdir(), "/temp.qs"))
-  statcandata <- qs::qread(qs_file)
-  
-}
-
-statcan_search <- function(keywords,lang)
-{
+statcan_search <- function(keywords,lang) {
   
   # Loading data
   if (lang == "eng")
